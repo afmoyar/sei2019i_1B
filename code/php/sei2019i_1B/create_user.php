6288788429bcd843e8a2cdf780624de4fd92f901
Cristian García <ccgarciab@unal.edu.co>
2019-07-07 15:02:28 -0500

    Modifying server behaviour to fit new client behaviour: empty string is used to signify succesfull insertion in DB.

--- code/php/sei2019i_1B/create_user.php
+++ code/php/sei2019i_1B/create_user.php
@@ -1,12 +1,21 @@
 <?php
-
 include 'connection.php';
+
 $id=$_POST['id'];
 $name=$_POST['name'];
 $password=$_POST['password'];
-
 $sql="INSERT INTO user VALUES('$id','$name','$password')";
-mysqli_query($connection,$sql) or die (mysqli_error());
+$result = mysqli_query($connection,$sql) or die (mysqli_error());
+
+if($result){
+
+    echo "";
+}
+else{
+
+    echo "error";
+}
+
 mysqli_close($connection);
 
 ?>
