f0f4ed233379ffc969230d6a0191eb545f0b93e2
Juan Pablo Betancourt Maldonado <43021811+JPBMTV@users.noreply.github.com>
2019-07-07 16:33:24 -0500

    Merge fix

--- code/MapApp/app/src/main/java/presentation/Activities/WelcomeAdminActivity.java
+++ code/MapApp/app/src/main/java/presentation/Activities/WelcomeAdminActivity.java
@@ -2,13 +2,14 @@ package presentation.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
+
+import android.view.View;
 import android.widget.Button;
-=======
->>>>>>> ShowAdminandUsernames
 import android.widget.TextView;
 
 import com.example.mapapp.R;
+
+import businessLogic.Controllers.AdminLoginController;
 
 public class WelcomeAdminActivity extends AppCompatActivity {
 
@@ -18,16 +19,22 @@ public class WelcomeAdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_admin);
-<<<<<<< HEAD
+
         TextView placeToSearch = (TextView) findViewById(R.id.placeToSearch);
         Button btnSignup=(Button) findViewById(R.id.pointToLocate);
-=======
+
         id = getIntent().getStringExtra("id");
         name = getIntent().getStringExtra("name");
-        TextView admin_id = (TextView) findViewById(R.id.textVadminid);
-        TextView admin_name = (TextView) findViewById(R.id.textVadminname);
+        final TextView admin_id = (TextView) findViewById(R.id.adminid);
+        final TextView admin_name = (TextView) findViewById(R.id.adminName);
         admin_id.setText(id);
         admin_name.setText(name);
->>>>>>> ShowAdminandUsernames
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AdminLoginController.login(getApplicationContext(),admin_id.getText().toString(),admin_name.getText().toString());
+            }
+        });
     }
 }
