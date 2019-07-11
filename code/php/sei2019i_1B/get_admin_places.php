723e813ce77dc66db93d5872642e3d24246b8cc5
sv1024 <vejars1024@gmail.com>
2019-07-11 02:34:29 -0500

    get_admin_places php not ready yet

--- code/php/sei2019i_1B/get_admin_places.php
+++ code/php/sei2019i_1B/get_admin_places.php
@@ -1,34 +1,17 @@
 <?php
 include 'connection.php';
-
-$query = "select count(*) from country where admin <> null";
+$query = "select count(*) from country where admin_id != ''";
 $result_count = $connection -> query($query);
-
 if($result_count == 0){
-    echo json_encode(array('error' => 0));
-    exit();
+    echo "error";
 }
 
-$query = "select name from country where admin <> null";
-$result_name = $connection -> query($query);
-$fields = array("name");
-
-$response["countries"] = array();
-
-while($country_row = $result_name -> fetch_array()){
-
-    $country = array();
-
-    foreach($fields as $field){
-    
-        $country[$field] = utf8_encode($country_row[$field]);
-    }
-    
-    $response["countries"][] = $country;
+$query = "select name from place where country_name in(select name from country where admin_id != '')";
+$response = array();
+while($row = $connection->database_fetch_assoc($query)){
+	$response[] = $row;
 }
 
-
-echo json_encode($response);
 $result_count -> close();
-$result_names -> close();
 ?>
+
