0cd9f19b25191521db42978d7090e8bb1f7c01fd
Juan Pablo Betancourt Maldonado <43021811+JPBMTV@users.noreply.github.com>
2019-07-20 16:26:35 -0500

    New Activities

---
+++ code/MapApp/app/src/main/java/presentation/Activities/AdminEventDateActivity.java
@@ -0,0 +1,15 @@
+package presentation.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mapapp.R;
+
+public class AdminEventDateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_event_date);
+    }
+}
