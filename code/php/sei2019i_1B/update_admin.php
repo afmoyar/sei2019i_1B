e8c47813579faa45fd13a4aebcc8202f00caf957
Juan Pablo Betancourt Maldonado <43021811+JPBMTV@users.noreply.github.com>
2019-07-21 15:52:50 -0500

    php fix

--- code/php/sei2019i_1B/update_admin.php
+++ code/php/sei2019i_1B/update_admin.php
@@ -3,7 +3,7 @@ include 'connection.php';
 
 $limit_date=$_POST['limit_date'];
 
-$consulta = "UPDATE administrator SET limit_date = '$limit_date' WHERE id = 'Otro'";
+$consulta = "UPDATE administrator SET limit_date = '$limit_date' WHERE id = 'Admin'";
 mysqli_query($connection,$consulta)or die (mysqli_error());
 mysqli_close($connection);
 
