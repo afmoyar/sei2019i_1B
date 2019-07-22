2ef42feadfbd427bab0ad1379d2a4b7e596513b4
Cristian García <ccgarciab@unal.edu.co>
2019-07-21 21:20:06 -0500

    Re-adding some imports that became relevant again after the merge.

--- code/MapApp/app/src/main/java/dataAccess/DataBase/Database.java
+++ code/MapApp/app/src/main/java/dataAccess/DataBase/Database.java
@@ -2,8 +2,11 @@ package dataAccess.DataBase;
 
 import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.RequestFuture;
